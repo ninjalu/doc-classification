40b3affb1d4cf856df4359d354d85ec5fdcdbb2f
Lu Luo <luolu.renmin@gmail.com>
2020-12-21 18:05:33 +0000

    presentation

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,6 @@
+from os import pread
 import torch
+import numpy as np
 from torch import Tensor
 from torch.utils.data import TensorDataset, DataLoader
 from typing import Dict, List, Tuple, Optional, Union
@@ -20,14 +22,18 @@ class NLPEnsemble():
         self.traditional_nlp.fit(X_train, y_train)
     
     def predict(self, X_test, test_dataloader):
-        self.t_nlp_pred = self.traditional_nlp.predict(X_test, test_dataloader)
-        self.nn_nlp_pred = _nn_predict(self.nn_nlp, test_dataloader)
-        assert len(self.t_nlp_pred) == len(self.nn_nlp_pred)
+        self.t_nlp_pred = self.traditional_nlp.predict_proba(X_test)
+        _, self.nn_nlp_pred = bert_predict(self.nn_nlp, test_dataloader)
+        print(self.t_nlp_pred)
+        print(self.t_nlp_pred.shape)
+        print(self.nn_nlp_pred)
+        print(self.nn_nlp_pred.shape)
         self.pred = self.ratio*self.t_nlp_pred + (1-self.ratio)*self.nn_nlp_pred
-        return self.pred
+        return np.argmax(self.pred)
 
-def _nn_predict(model, test_dataloader):
+def bert_predict(model, test_dataloader):
     pred = []
+    pred_logits = []
     model.eval()
     for batch in test_dataloader:
         input_ids, attention_masks, labels = batch
@@ -35,9 +41,12 @@ def _nn_predict(model, test_dataloader):
             output = model(
                 input_ids=input_ids,
                 token_type_ids=None,
-                attention_masks=attention_masks,
+                attention_mask=attention_masks,
                 labels=labels
             )
             logits = output[1]
+        pred_logits.append(logits.numpy().flatten())
         pred.append(torch.argmax(logits, dim=1).numpy())
-    return pred
+    pred_logits = np.array(pred_logits)
+    print(pred_logits)    
+    return pred, pred_logits
